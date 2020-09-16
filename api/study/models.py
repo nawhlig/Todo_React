84dce32d4fcd49ccc49017c6bc7c0463a5f217de
nawhlig <dksskd84@gmail.com>
2020-09-16 19:37:12 +0900

    직렬화(serialization) 유효성검사(validate)
    class UserSerializer(ModelSerializer) 부분 변경

--- api/study/models.py
+++ api/study/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 
+#######################################################################################
+#                       유저계정 조인할 때  임포트 방법                                 #
+# 일반적으로 조인하기 위해서 모델스의 유저 테이블 추가 하는 방법도 있지만                  #
+# 예)from account.models import User                                                  #
+# 장고에서 유저모델이 변경되는것을 고려해 만든 계정관련 모듈을 추가하면                    #
+# settings.py 나 common.py 에 AUTH_USER_MODEL 에 정의해논 유저모델을 가져올 수 있음      #
+# 예) AUTH_USER_MODEL = account.USER 로 정의하면                                      #
+#     from django.contrib.auth import get_user_model 로 선언해서 사용가능              #
+#######################################################################################
+from django.contrib.auth import get_user_model
+
+
 # Create your models here.
 
 
@@ -7,6 +19,10 @@ class Students(models.Model):
     name = models.CharField(max_length=10)
     address = models.CharField(max_length=50)
     email = models.CharField(max_length=30)
+    memo = models.CharField(max_length=300, null=True)
+    reg_user = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE
+    )  # 외래키 지정할 때 삭제했을 때의 규칙도 지정해 주어야 함!!
 
 
 class Scores(models.Model):
@@ -14,3 +30,4 @@ class Scores(models.Model):
     math = models.IntegerField()
     english = models.IntegerField()
     science = models.IntegerField()
+    reg_user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
--- api/study/views.py
+++ api/study/views.py
@@ -1,10 +1,16 @@
+from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from .models import Students, Scores
-from .serializers import StudentsSerializer, ScoresSerializer
-from rest_framework import status
-from rest_framework.decorators import api_view, action
-from rest_framework.views import APIView
+from .serializers import (
+    StudentsSerializer,
+    ScoresSerializer,
+    StudentBasicSerializer,
+    ScoresBasicSerializer,
+)
+from rest_framework import status, viewsets
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view, action
 from rest_framework.response import Response
 
 
@@ -17,63 +23,75 @@ class StudentView(ModelViewSet):
     queryset = Students.objects.all()
     serializer_class = StudentsSerializer
 
-    def get_queryset(self):
-        qs = super().get_queryset()
-        name = self.request.query_params.get("name")
-        if name:
-            qs = qs.filter(name=name)
-        return qs
+    # def get_queryset(self):
+    #     qs = super().get_queryset()
+    #     name = self.request.query_params.get("name")
+    #     math = self.request.query_params.get('math')
+    #     science = self.request.query_params.get('science')
+    #     if name:
+    #         qs = qs.filter(name=name)
+    #     if math:
+    #         qs = qs.filter(math__gt=math)
+    #     return qs
 
-    @action(detail=False, methods=["GET"])
-    def incheon(self, request):
-        qs = self.get_queryset().filter(address__contains="인천")  # like '%인천%'
-        serializer = self.get_serializer(qs, many=True)
-        return Response(serializer.data)
+    # #기본URL/incheon
+    # @action(detail=False, methods=["GET"])
+    # def incheon(self, request):
+    #     qs = self.get_queryset().filter(address__contains="인천")  # like '%인천%'
+    #     serializer = self.get_serializer(qs, many=True)
+    #     return Response(serializer.data)
 
-    @action(detail=True, methods=["PUT"])
-    def init(self, request, pk):
-        instance = self.get_object()
-        instance.address = ""
-        instance.save(update_fields=["address", "email"])
-        serializer = self.get_serializer(instance)
-        return Response(serializer.data)
+    # #기본URL/pk/init
+    # @action(detail=True, methods=["PUT"])
+    # def init(self, request, pk):
+    #     instance = self.get_object()
+    #     instance.address = ""
+    #     instance.email = ""
+    #     instance.save(update_fields=["address", "email"])
+    #     serializer = self.get_serializer(instance)
+    #     return Response(serializer.data)
 
 
 class ScoresView(ModelViewSet):
-    queryset = Scores.objects.all()
+    queryset = Scores.objects.all()  # 전체데이터를 조회하는
     serializer_class = ScoresSerializer
 
-    def get_queryset(self):
-        qs = super().get_queryset()
-        name = self.request.query_params.get("name")
-        math = self.request.query_params.get("math")
-        english = self.request.query_params.get("english")
-        science = self.request.query_params.get("science")
-        order = self.request.query_params.get("order")
-        if name:
-            qs = qs.filter(name=name)
-        if math:
-            qs = qs.filter(math__gte=math)
-        if english:
-            qs = qs.filter(english__gte=english)
-        if science:
-            qs = qs.filter(science__gte=science)
-        if order:
-            qs = qs.order_by(order)
-        return qs
+    # #오버라이딩
+    # def get_queryset(self):
+    #        #Scores.objects.all()
+    #     qs = super().get_queryset() # SELECT * FROM scores
+    #
+    #     name = self.request.query_params.get("name")
+    #     math = self.request.query_params.get("math")
+    #     english = self.request.query_params.get("english")
+    #     science = self.request.query_params.get("science")
+    #     order = self.request.query_params.get("order")
+    #     if name:
+    #         qs = qs.filter(name=name)   # SELECT * FROM scores WHERE name = name
+    #     if math:
+    #         qs = qs.filter(math__gte=math)
+    #     if english:
+    #         qs = qs.filter(english__gte=english)
+    #     if science:
+    #         qs = qs.filter(science__gte=science)
+    #     if order:
+    #         qs = qs.order_by(order)
+    #     return qs
 
-    @action(detail=False, methods=["GET"])
-    def top(self, request):
-        qs = self.get_queryset().filter(
-            math__gte=80, english__gte=80, science__gte=80
-        )  # 각각의 점수 모두 80점 이상
-        serializer = self.get_serializer(qs, many=True)
-        return Response(serializer.data)
+    # #PUT, DETAIL GET, DELETE (PK)
+    # #LIST, LIST 0, 1, >=
+    # @action(detail=False, methods=["GET"])
+    # def top(self, request):
+    #     qs = self.get_queryset().filter(
+    #         math__gte=80, english__gte=80, science__gte=80
+    #     )  # 각각의 점수 모두 80점 이상
+    #     serializer = self.get_serializer(qs, many=True)
+    #     return Response(serializer.data)
 
 
-###################################################################################################################
-#  CBV방식 : 클래스로 뷰를 만드는 방법                                                                                #
-###################################################################################################################
+####################################################################################################################
+##  CBV방식 : 클래스로 뷰를 만드는 방법                                                                              #
+####################################################################################################################
 # class StudentView(APIView):
 #     def get(self, request):
 #         qs = Students.objects.all()
@@ -153,8 +171,8 @@ class ScoresView(ModelViewSet):
 ###################################################################################################################
 #  FBV방식 : 함수로 뷰를 만드는 방법                                                                                #
 ###################################################################################################################
-# # get과 POST 방식으로 만들겠다.
-# @api_view(["GET", "POST"])
+
+# @api_view(["GET", "POST"])  # get과 POST 방식으로 만들겠다. 는 의미
 # def StudentView(request):
 #     if request.method == "GET":
 #         qs = Students.objects.all()
@@ -194,11 +212,12 @@ class ScoresView(ModelViewSet):
 # @api_view(["GET", "POST"])
 # def ScoreView(request):
 #     if request.method == "GET":
-#         qs = Scores.objects.all()
+#         qs = Scores.objects.all() #전체데이터 조회, 리스트형식이 반환 []
 #         serializer = ScoresSerializer(qs, many=True)
+#         print(serializer.data)
 #         return Response(serializer.data)
 #     elif request.method == "POST":
-#         serializer = ScoresSerializer(data=request.data)
+#         serializer = ScoresSerializer(data=request.data) #직렬화 생성, 딕셔너리 비슷한형태
 #         if serializer.is_valid():
 #             serializer.save()
 #             return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -223,3 +242,65 @@ class ScoresView(ModelViewSet):
 #     elif request.method == "DELETE":
 #         qs.delete()
 #         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+###################################################################################################################
+#  FBV방식 : 뷰의 기본형식으로 작성법                                                                                             #
+###################################################################################################################
+@api_view(["GET", "POST"])
+def StudentBasicView(request):
+    if request.method == "GET":
+        student = Students.objects.all()
+        serializer = StudentBasicSerializer(student, many=True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = StudentBasicSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()  # serializer.save()가 동작하면 serializers.py 의 def create(self, validated_date): 가 실행됨
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
+
+@api_view(["PUT"])
+def StudentBasicDetailView(request, pk):
+    if request.method == "PUT":
+        student = Students.objects.get(pk=pk)
+        serializer = StudentBasicSerializer(student, data=request.data)
+        # student 원래데이터
+        # request.data 사람이 보내준 데이터
+        if serializer.is_valid():
+            serializer.save()
+            # (원래데이터 <- 사람이 보내준 데이터) -> SAVE
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+
+
+@api_view(["GET", "POST"])
+def ScoresBasicView(request):
+    if request.method == "GET":
+        scores = Scores.objects.all()
+        serializer = ScoresBasicSerializer(scores, many=True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = ScoresBasicSerializer(data=request.data)
+        if serializer.is_valid():
+            # 가공
+            serializer.save(
+                memo="이건 테스트 입니다."
+            )  # serializer.save()가 동작하면 serializers.py 의 def create(self, validated_date): 가 실행됨
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
+
+@api_view(["PUT"])
+def ScoresBasicDetailView(request, pk):
+    if request.method == "PUT":
+        scores = Scores.objects.get(pk=pk)
+        # scores 원래데이터
+        # request.data 사람이 보내준 데이터
+        # (원래데이터 <- 사람이 보내준 데이터) -> SAVE
+        serializer = ScoresBasicSerializer(scores, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
