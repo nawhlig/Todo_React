1e3660b54e3500f59d48143c3133063e36c82ba9
nawhlig <dksskd84@gmail.com>
2020-09-18 10:27:51 +0900

    jsx 조건처리(3항,즉시)
    JSX문법중 조건처리
    3항연산
    즉시실행함수

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,9 +1,28 @@
-import React from 'react';
-import 'App.css';
-import Count from "Count";
-import CompoWelcome from "CompoWelcome";
+import React from 'react'
+import 'App.css'
+import Count from "Count"
+import CompoWelcome from "CompoWelcome"
 import CompoReact from "CompoReact"
 import Compo_Button1 from "Compo_Button1"
+
+function JsxTest() {
+  const [a, setA] = React.useState(false);
+  const user = 1;
+  return(
+    <>
+      {a && true && <div>참 일경우만 나오는</div>}
+      {a ? <div>참일경우</div> : <div>거짓말일경우</div>}
+
+      {
+        (() => {
+          //함수
+          if(user==1){return <div>User가 1</div>}
+          else if (user==2){return <div>User가 2</div>}
+        })()
+      }
+    </>
+  );
+}
 
 
 function App() {
@@ -13,6 +32,7 @@ function App() {
      <CompoWelcome/>
      <CompoReact/>
      <Compo_Button1/>
+     <JsxTest/>
     </div>
   );
 }
